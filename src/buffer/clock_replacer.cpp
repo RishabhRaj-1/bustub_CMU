2068e505f704ca3ee59302bba501e12c31a70214
l1997i <lil147@pitt.edu>
2020-05-17 21:16:45 +0800

    feat: write the Victim func

--- src/buffer/clock_replacer.cpp
+++ src/buffer/clock_replacer.cpp
@@ -14,12 +14,68 @@
 
 namespace bustub {
 
-ClockReplacer::ClockReplacer(size_t num_pages) {}
+ClockReplacer::ClockReplacer(size_t num_pages) {
+  clk_ptr = 0;                         /* initialize the clk_ptr, points to the first place */
+  buffer_size = num_pages;             /* The buffer size is the same number as num_pages */
+  reflag = vector<bool>(num_pages, 0); /* all the frames are not referred */
+  inflag = vector<bool>(num_pages, 0); /* all the frames are not in the ClockReplacer */
+}
 
 ClockReplacer::~ClockReplacer() = default;
 
-bool ClockReplacer::Victim(frame_id_t *frame_id) { return false; }
+/*
+ * Starting from the current position of clock hand, find the first frame that is
+ * both in the `ClockReplacer` and with its ref flag set to false. If a frame is in
+ * the `ClockReplacer`, but its ref flag is set to true, change it to false instead.
+ * This should be the only method that updates the clock hand.
+ */
+bool ClockReplacer::Victim(frame_id_t *frame_id) {
+  bool ret = false;   /* have NOT find the result in the beginning */
+  short candi = -1;   /* which frame to victim */
 
+  for (size_t i = 0; i < buffer_size; i++) {
+    short idx = (clk_ptr + i) % buffer_size;
+
+    /* IF find the first frame that is both in the `ClockReplacer`
+     * and with its ref flag set to false */
+    if (inflag[idx] && !reflag[idx]) {
+      clk_ptr = (idx + 1) % buffer_size; /* increase the clk_ptr by 1 */
+      *frame_id = idx;                   /* update the index of the new frame_id */
+      inflag[idx] = false;               /* new frame is not in the ClockReplacer */
+      ret = true;                        /* found the result, break immediately */
+      break;
+    }
+    /* ELSE IF a frame is in the `ClockReplacer`,
+     * but its ref flag is set to true, change it to false instead */
+    else if (inflag[idx] && reflag[idx]) {
+      reflag[idx] = false; /* change ref flag to false, victim later */
+      /* choose another candi to victim */
+      candi = (candi == -1) ? idx : candi; /* IF there's no candidate, the first idx is to victim */
+    }
+  }
+
+  /* IF have NOT found a result without the ref flag set to false */
+  if (!ret) {
+    /* IF there's NO candidates */
+    if (candi == -1) {
+      frame_id = nullptr;
+    }
+    /* ELSE there's one candidate with the ref flag set to true */
+    else {
+      clk_ptr = (candi + 1) % buffer_size;  /* update the clk_ptr */
+      *frame_id = candi;
+      inflag[candi] = false; /* the new frame is NOT in the ClockReplacer */
+      ret = true;            /* find a candidate, so change the result to true */
+    }
+  }
+
+  return ret;
+}
+
+/*
+ * This method should be called after a page is pinned to a frame in the BufferPoolManager.
+ * It should remove the frame containing the pinned page from the ClockReplacer.
+ */
 void ClockReplacer::Pin(frame_id_t frame_id) {}
 
 void ClockReplacer::Unpin(frame_id_t frame_id) {}
